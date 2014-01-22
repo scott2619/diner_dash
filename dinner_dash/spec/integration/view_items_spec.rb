d7422a95d5b31e78bdf6e2f4a894c722a54b8313
Charlie <ankothari@gmail.com>
2014-01-22 16:20:49 +0530

    Added spec for the first requirement, just copied the features

---
+++ dinner_dash/spec/integration/view_items_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+feature "Unauthenticated Users" do
+	scenario "can view all the items"
+	scenario "Browse items by category"
+  scenario "Add an item to my cart"
+  scenario "View my cart"
+  scenario "Remove an item from my cart"
+  scenario "Increase the quantity of a item in my cart"
+  scenario "Log in, which should not clear the cart"
+end
