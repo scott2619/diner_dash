a166be4eab373c4cb422d77ab6036df01a04986b
Charlie <ankothari@gmail.com>
2014-01-22 15:02:36 +0530

    Initial commit for the rails project with all the basic gems

---
+++ dinner_dash/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DinnerDash::Application.config.secret_key_base = '7ce4839bf7c21c4c0ed96718cece341d90f3fb8fedd607c4f1dda74852a14a64ff2bbe096635bcd5608afb1dde6d972ed67ed068c4ddaed5a15eeca689419ef2'
