b73b3233ce4cdfbfc39913c5eb26fa3d2787cda0
Charlie <ankothari@gmail.com>
2014-01-22 23:21:10 +0530

    Added the view item spec. Also set up the high_voltage and the factory for the item model

---
+++ dinner_dash/spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :item do
+    title "Computer"
+    description "Electronics"
+    price 2000
+    photo "http://www.google.com"
+	end
+end
--- dinner_dash/spec/spec_helper.rb
+++ dinner_dash/spec/spec_helper.rb
@@ -16,15 +16,7 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.include FactoryGirl::Syntax::Methods
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
