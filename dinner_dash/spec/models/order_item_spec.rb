4a823b6bd4569f7c6a20644b7afcac7534577078
Scott Hollinshead <scottspsp@hotmail.co.uk>
2014-01-23 12:46:39 +0000

    removed old specs

--- dinner_dash/spec/models/order_item_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe OrderItem do
-	it {should belong(:order)}
-	it { should has_one(:item)}
-	it { should respond_to(:quantity)}
-end
--- dinner_dash/spec/models/order_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Order do
-	it { should have_many(:order_items)}
-	it { should belong_to(:user)}
-	it { should respond_to(:status)}
-end
